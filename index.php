65e431a9d17a636fe87e6c7e1fa544803f98bf6d
Gagan0123 <gagan@iamme.in>
2013-09-13 15:08:38 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/*
+  Plugin Name: Shortcodes in Menus
+  Description: Allows you to add shortcodes in WordPress Navigation Menus
+  Plugin URI: http://gagan.pro
+  Version: 0.1
+  Author URI: http://gagan.pro
+  Author: Gagan0123
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Allows shortcode to be saved in database
+ */
+function gs_allow_saving_shortcode_custom_links( $url, $orig_url, $context ){
+	if($context=='db'){
+		return $orig_url;
+	}
+	return $url;
+}
+
+function gs_sw_security_check(){
+	if(current_user_can('activate_plugins')){
+		//Conditionally adding the function for database context for 
+		add_filter( 'clean_url', 'gs_allow_saving_shortcode_custom_links', 99, 3 );
+	}
+}
+add_action('wp_loaded','gs_sw_security_check');
+
+/**
+ * Allows shortcode to be processed and displayed
+ */
+function gs_allow_display_shortcode_custom_links( $url, $orig_url, $context ){
+	if( $context=='display' ){
+		return do_shortcode($orig_url);
+	}
+	return $url;
+}
+add_filter( 'clean_url', 'gs_allow_display_shortcode_custom_links', 1, 3 );
---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== Shortcodes in Menus ===
+Contributors: Gagan0123
+Tags: Shortcode, Menus, Custom Link
+Requires at least: 3.5
+Tested up to: 3.6.1
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Allows you to add shortcodes in WordPress Navigation Menus
+
+== Description ==
+
+Allows you to add shortcodes in WordPress Navigation Menus so that you can 
+generate the links dynamically.
+
+== Installation ==
+
+1. Add the plugin's folder in the WordPress' plugin directory.
+1. Activate the plugin.
+1. You can now add shortcodes in the custom links of the menus
+
+== Changelog ==
+0.1	Initial plugin
