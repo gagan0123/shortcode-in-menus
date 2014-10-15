af0a5d3d5c02c9682956ed93247fe90975c9f738
Gagan Deep Singh <me@gagan.pro>
2014-10-15 07:42:46 +0530

    Added functionality to add full HTML based ShortCodes in Menus instead of just URLs

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
   Plugin Name: Shortcodes in Menus
   Description: Allows you to add shortcodes in WordPress Navigation Menus
@@ -8,45 +9,64 @@
   Author: Gagan Deep Singh
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (!defined('ABSPATH'))
+    exit; // Exit if accessed directly
 
 /**
  * Allows shortcode to be saved in database
  */
-function gs_sim_allow_saving_shortcode_custom_links( $url, $orig_url, $context ){
-	if($context=='db'){
-		return $orig_url;
-	}
-	return $url;
+
+function gs_sim_allow_saving_shortcode_custom_links($url, $orig_url, $context) {
+    if ($context == 'db') {
+        return $orig_url;
+    }
+    return $url;
 }
 
-function gs_sim_security_check(){
-	if(current_user_can('activate_plugins')){
-		//Conditionally adding the function for database context for 
-		add_filter( 'clean_url', 'gs_sim_allow_saving_shortcode_custom_links', 99, 3 );
-	}
+function gs_sim_security_check() {
+    if (current_user_can('activate_plugins')) {
+        //Conditionally adding the function for database context for 
+        add_filter('clean_url', 'gs_sim_allow_saving_shortcode_custom_links', 99, 3);
+    }
 }
-add_action('wp_loaded','gs_sim_security_check');
+
+add_action('wp_loaded', 'gs_sim_security_check');
 
 /**
  * Allows shortcode to be processed and displayed
  */
-function gs_sim_allow_display_shortcode_custom_links( $url, $orig_url, $context ){
-	if( $context=='display' ){
-		return do_shortcode($orig_url);
-	}
-	return $url;
+function gs_sim_allow_display_shortcode_custom_links($url, $orig_url, $context) {
+    if ($context == 'display') {
+        return do_shortcode($orig_url);
+    }
+    return $url;
 }
-add_filter( 'clean_url', 'gs_sim_allow_display_shortcode_custom_links', 1, 3 );
+
+add_filter('clean_url', 'gs_sim_allow_display_shortcode_custom_links', 1, 3);
 
 /**
  * Adding a test shortcode for testing this plugin
- **/
-add_shortcode('gs_test_shortcode','gs_sim_test_shortcode');
+ * */
+add_shortcode('gs_test_shortcode', 'gs_sim_test_shortcode');
 
 /**
  * Returns "Hello World" for testing the shortcode
  */
-function gs_sim_test_shortcode($data){
+function gs_sim_test_shortcode($data) {
     return "http://gagan.pro";
 }
+
+if (!function_exists('gs_sim_menu_item')) {
+
+    /**
+     * Allows adding of full HTML supported shortcodes instead of just URL's
+     */
+    function gs_sim_menu_item($item_output, $item) {
+        if ($item->post_title == 'FULL HTML OUTPUT') {
+            $item_output = do_shortcode($item->url);
+        }
+        return $item_output;
+    }
+
+}
+add_filter('walker_nav_menu_start_el', 'gs_sim_menu_item', 10, 2);
--- readme.txt
+++ readme.txt
@@ -14,11 +14,17 @@ Allows you to add shortcodes in WordPress Navigation Menus
 Allows you to add shortcodes in WordPress Navigation Menus so that you can 
 generate the links dynamically.
 
+To test this, you can add a custom link with a ShortCode [gs_test_shortcode] as link URL, if it points to http://gagan.pro, plugin is working, if not(even any other ShortCode), then please write a new support ticket with ticket with the ShortCode you used and from which plugin/function that ShortCode is from, so that I'll be able to help you better.
+
+PLEASE NOTE: If you want to use a ShortCode that outputs not just the URL, but complete HTML sections, put "FULL HTML OUTPUT" in the "Link Text" option for that link and it will output the complete HTML without breaking your site.
+
 == Installation ==
 
 1. Add the plugin's folder in the WordPress' plugin directory.
 1. Activate the plugin.
-1. You can now add shortcodes in the custom links of the menus
+1. You can now add ShortCodes in the custom links of the menus.
+1. To test this, you can add a custom link with a ShortCode [gs_test_shortcode] as link, if it points to http://gagan.pro, plugin is working
+1. If you want to use a ShortCode that outputs not just the url, but complete HTML sections, please make use of the title 'FULL HTML OUTPUT' for that link and it will output the complete HTML without breaking your site.
 
 == Changelog ==
 
@@ -30,3 +36,6 @@ generate the links dynamically.
 
 = 1.1 =
 * Tested with WordPress 4.0
+
+= 1.2 =
+* Added ability to echo complete HTML output instead of just URL by using ShortCode
