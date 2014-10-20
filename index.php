1056b5b29bae5c02164379f32dc2ffa38c796b05
Saurabh Shukla <contact.saurabhshukla@gmail.com>
2014-10-21 00:15:31 +0530

    added metabox and fields added js

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
-
 /*
   Plugin Name: Shortcodes in Menus
   Description: Allows you to add shortcodes in WordPress Navigation Menus
   Plugin URI: http://wordpress.org/plugins/shortcode-in-menus/
-  Version: 1.2
+  Version: 1.1
   Author URI: http://gagan.pro
   Author: Gagan Deep Singh
  */
@@ -56,17 +55,96 @@ function gs_sim_test_shortcode($data) {
     return "http://gagan.pro";
 }
 
-if (!function_exists('gs_sim_menu_item')) {
+function gs_wp_nav_menu_setup() {
+    add_meta_box('add-html-section', __('Shortcode'), 'gs_wp_nav_menu_item_link_meta_box', 'nav-menus', 'side', 'default');
 
-    /**
-     * Allows adding of full HTML supported shortcodes instead of just URL's
-     */
-    function gs_sim_menu_item($item_output, $item) {
-        if ($item->post_title == 'FULL HTML OUTPUT') {
-            $item_output = do_shortcode($item->url);
+    // Register advanced menu items (columns)
+    add_filter('manage_nav-menus_columns', 'gs_wp_nav_menu_manage_columns');
+}
+
+add_action('admin_init', 'gs_wp_nav_menu_setup');
+
+function gs_wp_nav_menu_item_link_meta_box() {
+    global $_nav_menu_placeholder, $nav_menu_selected_id;
+
+    $_nav_menu_placeholder = 0 > $_nav_menu_placeholder ? $_nav_menu_placeholder - 1 : -1;
+    
+    $last_object_id = get_option('gs_sim_last_object_id',0);
+    echo $last_object_id;
+    $object_id = (int)$last_object_id;
+    $object_id++;
+    echo $object_id;
+    $object_id = ($object_id<1)? 1 : $object_id;
+    echo $object_id;
+    update_option('gs_sim_last_object_id', $object_id);
+    ?>
+    <div class="gs-sim-div" id="gs-sim-div">
+        <input type="hidden" class="menu-item-db-id" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-db-id]" value="0" />
+        <input type="hidden" class="menu-item-object-id" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-object-id]" value="<?php echo $object_id; ?>" />
+        <input type="hidden" class="menu-item-object" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-object]" value="gs_sim" />
+        <input type="hidden" class="menu-item-type" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-type]" value="gs_sim" />
+        <p id="menu-item-title-wrap">
+            <label class="howto" for="custom-menu-item-title">
+                <span><?php _e('Title'); ?></span>
+                <input id="gs-sim-title" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-title]" type="text" class="regular-text menu-item-textbox input-with-default-title" title="<?php esc_attr_e('Title'); ?>" />
+            </label>
+        </p>
+        
+        <p id="menu-item-html-wrap">
+            <label class="howto" for="custom-menu-item-html">
+                <span><?php _e('HTML (with shortcode)'); ?></span>
+                <textarea style="width:100%;" rows="3" id="gs-sim-html" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-description]" class="code menu-item-textbox input-with-default-title" title="<?php esc_attr_e('Add text or html here. You can include shortcodes, as well!'); ?>"></textarea>
+            </label>
+        </p>
+
+        <p class="button-controls">
+            <span class="add-to-menu">
+                <input type="submit"<?php wp_nav_menu_disabled_check($nav_menu_selected_id); ?> class="button-secondary submit-add-to-menu right" value="<?php esc_attr_e('Add to Menu'); ?>" name="add-gs-sim-menu-item" id="submit-gs-sim" />
+                <span class="spinner"></span>
+            </span>
+        </p>
+
+    </div><!-- /.customlinkdiv -->
+    <?php
+}
+
+function gs_wp_nav_menu_manage_columns() {
+    
+}
+
+function gs_scripts_method($hook) {
+    if ('nav-menus.php' != $hook)
+        return;
+    wp_enqueue_script(
+            'gs-sim-admin', plugins_url('/js/admin.js', __FILE__), array('nav-menu')
+    );
+}
+
+add_action('admin_enqueue_scripts', 'gs_scripts_method');
+
+
+add_filter('walker_nav_menu_start_el', 'gs_sim_menu_item', 10, 2);
+
+function gs_sim_menu_item($item_output, $item) {
+    
+    if ($item->object != 'gs_sim') {
+        // legacy support
+        if ($item->post_title != 'FULL HTML OUTPUT') {
+            return $item_output;
         }
+    }
+    var_dump($item);
+
+    // legacy support
+    if ($item->post_title == 'FULL HTML OUTPUT') {
+        $item_output = do_shortcode($item->url);
         return $item_output;
     }
 
+
+    if ($item->object == 'gs_sim') {
+        $item_output = do_shortcode($item->description);
+        return $item_output;
+    }
+    return $item_output;
 }
-add_filter('walker_nav_menu_start_el', 'gs_sim_menu_item', 10, 2);
