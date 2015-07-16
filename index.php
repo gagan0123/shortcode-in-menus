a7d58dd7ecafbea766b13ebfae3eaa0582322dd8
Gagan Deep Singh <me@gagan.pro>
2015-07-16 21:00:37 +0530

    Fixed the error message that was popping up while adding new ShortCode and removed the admin.min.js as this file is not used on the frontend of the site, so we can deal with the original source

--- index.php
+++ index.php
@@ -42,6 +42,8 @@ if ( !class_exists( 'gsShortCodeInMenu' ) ) {
 
 			// filter the output when shortcode is saved using custom links, for legacy support
 			add_filter( 'clean_url', array( $this, 'display_shortcode' ), 1, 3 );
+
+			add_action( 'wp_ajax_add-menu-item', array( $this, 'ajax_add_menu_item' ), 0 );
 		}
 
 		/**
@@ -268,6 +270,83 @@ if ( !class_exists( 'gsShortCodeInMenu' ) ) {
 				return do_shortcode( $orig_url );
 			}
 			return $url;
+		}
+
+		/**
+		 * Ajax handler for add menu item request
+		 */
+		public function ajax_add_menu_item() {
+
+			check_ajax_referer( 'add-menu_item', 'menu-settings-column-nonce' );
+
+			if ( !current_user_can( 'edit_theme_options' ) )
+				wp_die( -1 );
+
+			require_once ABSPATH . 'wp-admin/includes/nav-menu.php';
+
+			// For performance reasons, we omit some object properties from the checklist.
+			// The following is a hacky way to restore them when adding non-custom items.
+
+			$menu_items_data = array();
+			foreach ( (array) $_POST[ 'menu-item' ] as $menu_item_data ) {
+				if (
+				!empty( $menu_item_data[ 'menu-item-type' ] ) &&
+				'custom' != $menu_item_data[ 'menu-item-type' ] &&
+				'gs_sim' != $menu_item_data[ 'menu-item-type' ] &&
+				!empty( $menu_item_data[ 'menu-item-object-id' ] )
+				) {
+					switch ( $menu_item_data[ 'menu-item-type' ] ) {
+						case 'post_type' :
+							$_object = get_post( $menu_item_data[ 'menu-item-object-id' ] );
+							break;
+
+						case 'taxonomy' :
+							$_object = get_term( $menu_item_data[ 'menu-item-object-id' ], $menu_item_data[ 'menu-item-object' ] );
+							break;
+					}
+
+					$_menu_items = array_map( 'wp_setup_nav_menu_item', array( $_object ) );
+					$_menu_item	 = reset( $_menu_items );
+
+					// Restore the missing menu item properties
+					$menu_item_data[ 'menu-item-description' ] = $_menu_item->description;
+				}
+
+				$menu_items_data[] = $menu_item_data;
+			}
+
+			$item_ids = wp_save_nav_menu_items( 0, $menu_items_data );
+			if ( is_wp_error( $item_ids ) )
+				wp_die( 0 );
+
+			$menu_items = array();
+
+			foreach ( (array) $item_ids as $menu_item_id ) {
+				$menu_obj = get_post( $menu_item_id );
+				if ( !empty( $menu_obj->ID ) ) {
+					$menu_obj		 = wp_setup_nav_menu_item( $menu_obj );
+					$menu_obj->label = $menu_obj->title; // don't show "(pending)" in ajax-added items
+					$menu_items[]	 = $menu_obj;
+				}
+			}
+
+			/** This filter is documented in wp-admin/includes/nav-menu.php */
+			$walker_class_name = apply_filters( 'wp_edit_nav_menu_walker', 'Walker_Nav_Menu_Edit', $_POST[ 'menu' ] );
+
+			if ( !class_exists( $walker_class_name ) )
+				wp_die( 0 );
+
+			if ( !empty( $menu_items ) ) {
+				$args = array(
+					'after'			 => '',
+					'before'		 => '',
+					'link_after'	 => '',
+					'link_before'	 => '',
+					'walker'		 => new $walker_class_name,
+				);
+				echo walk_nav_menu_tree( $menu_items, 0, (object) $args );
+			}
+			wp_die();
 		}
 
 	}
