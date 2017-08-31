5453826be575d7d79822a799a63385d599da8f29
Gagan Deep Singh <me@gagan.pro>
2017-08-31 15:41:09 +0530

    Changing version number before final release

--- shortcode-in-menus.php
+++ shortcode-in-menus.php
@@ -4,7 +4,7 @@
  * Plugin Name: Shortcodes in Menus
  * Description: Allows you to add shortcodes in WordPress Navigation Menus
  * Plugin URI: http://wordpress.org/plugins/shortcode-in-menus/
- * Version: 3.2
+ * Version: 3.3
  * Author: <a href="https://gagan0123.com">Gagan Deep Singh</a> and <a href="http://hookrefineandtinker.com">Saurabh Shukla</a>
  * Author URI: https://gagan0123.com
  * Text Domain: shortcode-in-menus
