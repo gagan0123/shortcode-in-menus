89245d5070be38b709101c3bac5769d2b92ac4cb
Gagan Deep Singh <me@gagan.pro>
2019-02-21 19:47:00 +0530

    PHPCS comment format change

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,7 +12,8 @@ if ( ! $_tests_dir ) {
 }
 
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // WPCS: XSS ok.
+	// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL;
 	exit( 1 );
 }
 
